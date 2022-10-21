3a9390058874f7217b7a6aa4debc8a803857eb29
agungSeptiana <203040102@mail.unpas.ac.id>
2022-10-21 17:49:05 +0700

    Menambahkan Post Category & Eloquent Relationship

--- application/prakweb_b_203040102_laravel/resources/views/posts.blade.php
+++ application/prakweb_b_203040102_laravel/resources/views/posts.blade.php
@@ -3,6 +3,7 @@
 @section('container')
     <article>
         <h1 class="mb-1">{{ $post->title }}</h1>
+        <p>By. Agung Septiana in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
         {!! $post->body !!}
     </article>
     <a href="/post">Back to Posts</a>
