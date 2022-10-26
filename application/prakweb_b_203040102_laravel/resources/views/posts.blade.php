75dc8e585225cf63fdb9780c10172eda0b888952
agungSeptiana <203040102@mail.unpas.ac.id>
2022-10-26 11:38:28 +0700

    Video 9 (Database Seeder)

--- application/prakweb_b_203040102_laravel/resources/views/posts.blade.php
+++ application/prakweb_b_203040102_laravel/resources/views/posts.blade.php
@@ -3,8 +3,8 @@
 @section('container')
     <article>
         <h1 class="mb-1">{{ $post->title }}</h1>
-        <p>By. Agung Septiana in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+        <p>By <a href="#" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
         {!! $post->body !!}
     </article>
-    <a href="/post">Back to Posts</a>
+    <a href="/post" class="d-block mt-3 text-decoration-none">Back to Posts</a>
 @endsection
