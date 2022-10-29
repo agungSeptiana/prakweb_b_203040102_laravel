ba8148a2ddd4a7813b6031dd39af10264e9bfb3a
agungSeptiana <203040102@mail.unpas.ac.id>
2022-10-29 15:18:07 +0700

    Menambahkan video 14 (View Login & Registration)

--- application/prakweb_b_203040102_laravel/resources/views/layouts/main.blade.php
+++ application/prakweb_b_203040102_laravel/resources/views/layouts/main.blade.php
@@ -7,7 +7,10 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    {{-- My style --}}
     <link rel="stylesheet" href="css/style.css">
+    {{-- Bootstrap icon --}}
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
 
     <title>AGS Blogs | {{ $title }}</title>
   </head>
