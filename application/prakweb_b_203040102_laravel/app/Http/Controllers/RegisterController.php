3e883501c2c3e2ae5aa17ee477b9ae797c7bf1a2
agungSeptiana <203040102@mail.unpas.ac.id>
2022-10-30 15:36:10 +0700

    Menambahkan video 15 (User Registration)

--- application/prakweb_b_203040102_laravel/app/Http/Controllers/RegisterController.php
+++ application/prakweb_b_203040102_laravel/app/Http/Controllers/RegisterController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class RegisterController extends Controller
 {
@@ -11,5 +13,23 @@ class RegisterController extends Controller
             'title' => 'Register',
             'active' => 'register'
         ]);
+    }
+
+    public function store(Request $request) {
+        $validatedData = $request->validate([
+            'name' => 'required|max:255',
+            'username' => ['required', 'min:3', 'max:255', 'unique:users'],
+            'email' => 'required|email:dns|unique:users',
+            'password' => 'required|min:5|max:255'
+        ]);
+
+        // $validatedData['password'] = bcrypt($validatedData['password']);
+        $validatedData['password'] = Hash::make($validatedData['password']);
+
+        User::create($validatedData);
+
+        // $request->session()->flash('success', 'Registation seccessfull! Please login');
+
+        return redirect('/login')->with('success', 'Registation seccessfull! Please login');
     }
 }
--- application/prakweb_b_203040102_laravel/resources/views/login/index.blade.php
+++ application/prakweb_b_203040102_laravel/resources/views/login/index.blade.php
@@ -3,6 +3,12 @@
 
     <div class="row justify-content-center">
         <div class="col-md-4">
+            @if (session()->has('success'))
+                <div class="alert alert-success mt-5 mb-4 alert-dismissible fade show" role="alert">
+                    {{ session('success') }}
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            @endif
             <main class="form-signin">
                 <h1 class="h3 mt-5 mb-4 fw-normal text-center text-light">Please Login</h1>
                 <form>
