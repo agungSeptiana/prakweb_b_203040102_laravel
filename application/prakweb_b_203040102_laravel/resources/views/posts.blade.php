5fa4516ea9f46f70504de60acad3505caf28fd4e
agungSeptiana <203040102@mail.unpas.ac.id>
2022-11-03 19:55:09 +0700

    Menambahkan vide 20 (Update & Delete Post)

--- application/prakweb_b_203040102_laravel/resources/views/posts.blade.php
+++ application/prakweb_b_203040102_laravel/resources/views/posts.blade.php
@@ -33,32 +33,28 @@
                 <a href="/post/{{ $posts[0]->slug }}" class="text-decoration-none btn btn-primary">Read more</a>
                 </div>
             </div> 
-        
-
-
-        <div class="container">
-            <div class="row">
-                @foreach ($posts->skip(1) as $post)
-                    <div class="col-md-4 mb-3">
-                        <div class="card" style="background-color: rgba(255, 255, 255, 0.2); box-shadow: 0 15px 35px rgba(0,0,0,0.05); backdrop-filter: blur(10px); border: 1px solid rgba(255,255,255,0.25); border-top: 1px solid rgba(255,255,255,0.5); border-left: 1px solid rgba(255,255,255,0.5);">
-                            <div class="position-absolute px-3 py-2" style="background-color: rgba(255, 255, 255, 0.2)"><a href="/posts?category={{ $post->category->slug }}" class="text-light text-decoration-none">{{ $post->category->name }}</a></div>
-                            <img src="https://source.unsplash.com/500x400?{{ $post->category->name }}" class="card-img-top" alt="{{ $post->category->name }}">
-                            <div class="card-body">
-                            <h5 class="card-title text-light"><a href="/post/{{ $post->slug }}" class="text-decoration-none text-light">{{ $post->title }}</a></h5>
-                            <p class="text-light">
-                                <small>
-                                    By <a class="text-light text-decoration" href="/posts?author={{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> {{ $post->created_at->diffForHumans() }}
-                                </small>
-                            </p>
-                            <p class="card-text text-light">{{ $post->excerpt }}</p>
-                            <a href="/post/{{ $post->slug }}" class="text-decoration-none btn btn-primary">Read more</a>
+            <div class="container">
+                <div class="row">
+                    @foreach ($posts->skip(1) as $post)
+                        <div class="col-md-4 mb-3">
+                            <div class="card" style="background-color: rgba(255, 255, 255, 0.2); box-shadow: 0 15px 35px rgba(0,0,0,0.05); backdrop-filter: blur(10px); border: 1px solid rgba(255,255,255,0.25); border-top: 1px solid rgba(255,255,255,0.5); border-left: 1px solid rgba(255,255,255,0.5);">
+                                <div class="position-absolute px-3 py-2" style="background-color: rgba(255, 255, 255, 0.2)"><a href="/posts?category={{ $post->category->slug }}" class="text-light text-decoration-none">{{ $post->category->name }}</a></div>
+                                <img src="https://source.unsplash.com/500x400?{{ $post->category->name }}" class="card-img-top" alt="{{ $post->category->name }}">
+                                <div class="card-body">
+                                <h5 class="card-title text-light"><a href="/post/{{ $post->slug }}" class="text-decoration-none text-light">{{ $post->title }}</a></h5>
+                                <p class="text-light">
+                                    <small>
+                                        By <a class="text-light text-decoration" href="/posts?author={{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> {{ $post->created_at->diffForHumans() }}
+                                    </small>
+                                </p>
+                                <p class="card-text text-light">{{ $post->excerpt }}</p>
+                                <a href="/post/{{ $post->slug }}" class="text-decoration-none btn btn-primary">Read more</a>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                @endforeach
+                    @endforeach
+                </div>
             </div>
-        </div>
-
         @else
             <p class="text-center fs-4 text-light">No Post Found.</p>
         @endif
