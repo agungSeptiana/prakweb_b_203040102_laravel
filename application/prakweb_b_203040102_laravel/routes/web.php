57fd58e33d7754e647e6a0e599827f1c001b6d1e
agungSeptiana <203040102@mail.unpas.ac.id>
2022-11-04 10:23:28 +0700

    Menambahkan video 23 (Authorization)

--- application/prakweb_b_203040102_laravel/routes/web.php
+++ application/prakweb_b_203040102_laravel/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminCategoryController;
 use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
@@ -22,8 +23,7 @@ use App\Http\Controllers\DashboardPostController;
 Route::get('/', function () {
     return view('home', [
         "title" => "home",
-        "active" => "home",
-        "image" => "kirito.png"
+        "active" => "home"
     ]);
 });
 
@@ -60,3 +60,5 @@ Route::get('/dashboard', function() {
 
 Route::get('/dashboard/posts/checkSlug', [DashboardPostController::class, 'checkSlug'])->middleware('auth');
 Route::resource('/dashboard/posts', DashboardPostController::class)->middleware('auth');
+
+Route::resource('/dashboard/categories', AdminCategoryController::class)->except('show')->middleware(('admin'));
